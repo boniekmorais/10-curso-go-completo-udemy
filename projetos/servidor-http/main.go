08e44374e32be8f301ac0cd3e197fcbbc28d484c
Boniek Morais <boniek.morais@gmail.com>
2023-11-15 17:56:08 -0300

    Atualizando passagem de função como parâmetro

--- projetos/servidor-http/main.go
+++ projetos/servidor-http/main.go
@@ -5,15 +5,29 @@ import (
 	"net/http"
 )
 
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+}
+
+func users(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello Users"))
+}
+
 func main() {
 
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
+	// http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello World"))
+	// })
 
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Users"))
-	})
+	// http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+	// 	w.Write([]byte("Hello Users"))
+	// })
+
+	// Usa a funcao declarada acima
+	http.HandleFunc("/home", home)
+
+	// Usa a funcao declarada acima
+	http.HandleFunc("/users", users)
 
 	log.Fatal(http.ListenAndServe(":5001", nil))
 }
