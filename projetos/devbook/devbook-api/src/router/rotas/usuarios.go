565b9970532de19e0de1f39f89c29b841f7a7f13
Boniek Morais <boniek.morais@gmail.com>
2023-11-23 13:54:46 -0300

    Ajuste da configuração de rotas

--- projetos/devbook/devbook-api/src/router/rotas/usuarios.go
+++ projetos/devbook/devbook-api/src/router/rotas/usuarios.go
@@ -1,46 +1,39 @@
 package rotas
 
-import "net/http"
+import (
+	"api/src/controller"
+	"net/http"
+)
 
 var rotasUsuarios = []Rota{
 	{
-		URI:    "/usuarios",
-		Metodo: http.MethodPost,
-		Funcao: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:                "/usuarios",
+		Metodo:             http.MethodPost,
+		Funcao:             controller.CriarUsuario,
 		RequerAutenticacao: false,
 	},
 	{
-		URI:    "/usuarios",
-		Metodo: http.MethodGet,
-		Funcao: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:                "/usuarios",
+		Metodo:             http.MethodGet,
+		Funcao:             controller.ListarUsuarios,
 		RequerAutenticacao: false,
 	},
 	{
-		URI:    "/usuarios/{id}",
-		Metodo: http.MethodGet,
-		Funcao: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:                "/usuarios/{id}",
+		Metodo:             http.MethodGet,
+		Funcao:             controller.ListarUsuarioPorId,
 		RequerAutenticacao: false,
 	},
 	{
-		URI:    "/usuarios/{id}",
-		Metodo: http.MethodPut,
-		Funcao: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:                "/usuarios/{id}",
+		Metodo:             http.MethodPut,
+		Funcao:             controller.AtualizarUsuario,
 		RequerAutenticacao: false,
 	},
 	{
-		URI:    "/usuarios/{id}",
-		Metodo: http.MethodDelete,
-		Funcao: func(w http.ResponseWriter, r *http.Request) {
-
-		},
+		URI:                "/usuarios/{id}",
+		Metodo:             http.MethodDelete,
+		Funcao:             controller.RemoverUsuario,
 		RequerAutenticacao: false,
 	},
 }
